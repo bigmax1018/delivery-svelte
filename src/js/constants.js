c8a53868cf3f94baf31632270a3fe29c1bfeaec7
bigmax1018 <152480620+bigmax1018@users.noreply.github.com>
2024-02-06 15:33:34 +0900

    update project and config

--- src/js/constants.js
+++ src/js/constants.js
@@ -4,12 +4,12 @@ export const USERROLE_OWNER = 2;
 
 // Constants for AppWrite
 export const APPWRITE_ENDPOINT = "https://cloud.appwrite.io/v1";
-export const APPWRITE_PROJECT = "6585ed8fe4c3bcc001fb";
-export const APPWRITE_DATABASE_ID = "65869eefc6dd2e5c0917";
-export const APPWRITE_FOOD_COLLECTION_ID = "65a8b3e2ac8601166b5e";
-export const APPWRITE_FOOD_STORAGE_ID = "65869debdfc84bdec4a1";
-export const APPWRITE_ORDER_COLLECTION_ID = "65b4e4a50c28409ec4c8";
-export const APPWRITE_USRE_LIST_COLLECTION_ID = "65bd0bc74999b6e54c81";
+export const APPWRITE_PROJECT = "65c121d6a60e8c1efac0";
+export const APPWRITE_DATABASE_ID = "65c129a3a4509c4174f9";
+export const APPWRITE_FOOD_COLLECTION_ID = "65c129d1a54e4481463a";
+// export const APPWRITE_FOOD_STORAGE_ID = "65869debdfc84bdec4a1";
+export const APPWRITE_ORDER_COLLECTION_ID = "65c129c5c5f485af3abe";
+export const APPWRITE_USRE_LIST_COLLECTION_ID = "65c129b5d14860b83d99";
 // export const APPWRITE_APIKEY = "AdminAPI";
 
 
